a9ec7134e2c1fe4ef5d6533057295441d65d8f99
Enzo Braga <ebdalmoro@gmail.com>
2023-11-13 12:00:24 -0300

    Add SlideBar

--- main.js
+++ main.js
@@ -9,35 +9,38 @@ renderer.outputEnconding = THREE.sRGBEncoding;
 
 const scene = new THREE.Scene();
 
-var img = new Image();
-img.onload = function () {
-	scene.background = new THREE.TextureLoader().load(img.src);
-	
-};
-img.src = "space.jpg";
+
+
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
 camera.position.set(0,0,5)
 
-const loader = new GLTFLoader();
-loader.load('earth2.glb', function(gltf) {scene.add(gltf.scene);})
+var loader = new THREE.Loader(); // Whichever ThreeJS loader you are using
+var modelPaths = ['./earth2.glb', './POLY.gbl']; // Array to store all model paths
+var currentPathIndex = 0; // index in the path array of the currently showing model
+var currentModel; // stores the model that is currently displaying
+loadModel(currentPathIndex);
+
+function loadModel(index){
+	if(currentModel){
+		scene.remove(currentModel); //remove the current model
+	}
+
+	loader.load(modelPaths[index], (result) => {
+		currentModel = result;
+		scene.add(result);
+	} );
+}
+
+function next(){
+  currentPathIndex ++;
+  loadModel(currentPathIndex);
+}
 
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enablePan = false;
 controls.enableZoom = false;
 controls.autoRotate = true;
-
-const light1 = new THREE.PointLight(0xffffff, 20, 100);
-light1.position.set( 50,30, 50);
-scene.add(light1);
-
-const light2 = new THREE.PointLight(0xffffff, 10, 100);
-light2.position.set( -50,30, 50);
-scene.add(light2);
-
-const light3 = new THREE.PointLight(0xffffff, 2, 100);
-light3.position.set( 0,30, -5);
-scene.add(light3);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 15);
 scene.add (ambientLight);
@@ -49,3 +52,28 @@ function animate(){
 }
 
 animate()
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; 
+
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+function checkmodel(){
+    if(slider.value ==  50){
+        model = 'earth2.glb';
+
+    } else {
+        model = '';
+    }
+}
+
+ const check = setInterval(checkmodel, 1);
+
+    if(slider.value = 60){
+        console.log(1);
+    }
